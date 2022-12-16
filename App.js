daea4bb5a112a22dbaa422cffdbd7b7ca1070e77
A <>
2022-12-16 14:58:56 +0000

    no message

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
-import React, {useState} from 'react';
-import {StyleSheet} from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet } from 'react-native';
 import {
   ViroARScene,
   ViroText,
   ViroConstants,
   ViroARSceneNavigator,
+  Viro3DObject,
+  ViroAmbientLight,
+  ViroSpotLight,
 } from '@viro-community/react-viro';
 
 const HelloWorldSceneAR = () => {
@@ -12,20 +15,26 @@ const HelloWorldSceneAR = () => {
 
   function onInitialized(state, reason) {
     console.log('guncelleme', state, reason);
-    if (state === ViroConstants.TRACKING_NORMAL) {
-      setText('Hello World!');
-    } else if (state === ViroConstants.TRACKING_NONE) {
-      // Handle loss of tracking
-    }
+    //if (state === ViroConstants.TRACKING_NORMAL) {
+    setText('Hello Worldx!');
+    //} else if (state === ViroConstants.TRACKING_NONE) {
+    // Handle loss of tracking
+    //}
   }
-
+  //https://github.com/ViroCommunity/samples/raw/main/ARDrivingCar/src/assets/car_body.vrx
   return (
     <ViroARScene onTrackingUpdated={onInitialized}>
-      <ViroText
-        text={text}
-        scale={[0.5, 0.5, 0.5]}
-        position={[0, 0, -1]}
-        style={styles.helloWorldTextStyle}
+
+      <ViroAmbientLight color={"#aaaaaa"} />
+      <ViroSpotLight innerAngle={5} outerAngle={90} direction={[0, -1, -.2]} position={[0, 3, 1]} color="#ffffff" castsShadow={true} />
+
+      <Viro3DObject
+        source={{ uri: 'https://firebasestorage.googleapis.com/v0/b/tour-2e13f.appspot.com/o/AOyllz5D5GQSVjiQXAee%2F3dmodels%2Fcar_body.vrx?alt=media&token=d9dcf7b9-7c33-4868-b3c4-7aef4eb112b4' }}
+
+        position={[0, -1, -2]}
+        scale={[.01, .01, .01]}
+        type="VRX"
+        dragType="FixedDistance" onDrag={() => { }}
       />
     </ViroARScene>
   );
@@ -44,7 +53,7 @@ export default () => {
 };
 
 var styles = StyleSheet.create({
-  f1: {flex: 1},
+  f1: { flex: 1 },
   helloWorldTextStyle: {
     fontFamily: 'Arial',
     fontSize: 30,
