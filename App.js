7b7a05c1fade17f0a78009472e946688df4fda09
A <>
2023-02-08 22:29:37 +0000

    no message

--- App.js
+++ App.js
@@ -52,7 +52,7 @@ export default function ARControl(props) {
         uri: 'https://github.com/andrewjb123/starter-kit/raw/master/balloon/balloon.vrx',
         resources:
           [
-            { uri: 'https://github.com/andrewjb123/starter-kit/raw/master/balloon/balloon.fbm' }
+            { uri: 'https://github.com/andrewjb123/starter-kit/raw/master/balloon/ballon.fbm/ballon.fbm' }
             /*{ uri: 'https://firebasestorage.googleapis.com/v0/b/tour-2e13f.appspot.com/o/3dmodels%2Fballoon%2F6e0c84fe7d202c3ac87b0c7f654fce74bf99881c.png?alt=media' },
             { uri: 'https://firebasestorage.googleapis.com/v0/b/tour-2e13f.appspot.com/o/3dmodels%2Fballoon%2Fbdb3d95033b62647d04a45566d4119de2b4f3325.png?alt=media' },
             { uri: 'https://firebasestorage.googleapis.com/v0/b/tour-2e13f.appspot.com/o/3dmodels%2Fballoon%2Faerial_beach_01_diff_8k.jpg?alt=media' },
@@ -81,7 +81,7 @@ export default function ARControl(props) {
         uri: 'https://github.com/andrewjb123/starter-kit/raw/master/balloon/balloon.vrx',
         resources:
           [
-            { uri: 'https://github.com/andrewjb123/starter-kit/raw/master/balloon/balloon.fbm' }
+            { uri: 'https://github.com/andrewjb123/starter-kit/raw/master/balloon/ballon.fbm/ballon.fbm' }
             /*{ uri: 'https://firebasestorage.googleapis.com/v0/b/tour-2e13f.appspot.com/o/3dmodels%2Fballoon%2F6e0c84fe7d202c3ac87b0c7f654fce74bf99881c.png?alt=media' },
             { uri: 'https://firebasestorage.googleapis.com/v0/b/tour-2e13f.appspot.com/o/3dmodels%2Fballoon%2Fbdb3d95033b62647d04a45566d4119de2b4f3325.png?alt=media' },
             { uri: 'https://firebasestorage.googleapis.com/v0/b/tour-2e13f.appspot.com/o/3dmodels%2Fballoon%2Faerial_beach_01_diff_8k.jpg?alt=media' },
